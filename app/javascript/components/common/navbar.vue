716b91ff2ad1fa1ad05773e24ebaa51c15a8f39c
Jeremy Billote <jbillote@gmail.com>
2017-12-22 14:59:12 -0800

    Add nav bar to all pages

---
+++ app/javascript/components/common/navbar.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class='text-center'>
+        <p><router-link to='/'>Home</router-link></p>
+        <p><router-link to='/about'>About</router-link></p>
+        <p><router-link to='/user'>User Lookup</router-link></p>
+    </div>
+</template>
